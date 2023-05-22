005ad0e281353818091ffb4ee3c24e386882285e
Kyle Close <k.james.close@gmail.com>
2023-05-21 22:26:53 -0400

    Header layout finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 
+import "../styles/Header.css"
+import companyIcon from "../img/company-icon.svg";
+import shopIcon from "../img/shop.svg"
+import checkoutIcon from "../img/checkout.svg"
+
 function Header() {
     return (
-        <div>Header Section</div>
+        <div className="sticky-header">
+            <div className="content">
+                <img className="icon" src={companyIcon} width={'70px'} />
+                <h1>The Divot Den</h1>
+                <div className="shop-checkout-icons">
+                    <img className="icon" src={shopIcon} width={"50px"} />
+                    <img className="icon checkout-icon" src={checkoutIcon} width={"45px"} />
+                </div>
+            </div>
+        </div>
     )
 }
 
