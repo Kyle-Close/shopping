f4d2fd42e35563a994aef48ae00933802022432b
Kyle-Close <k.james.close@gmail.com>
2023-05-23 15:57:05 -0700

    Most up to date version

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
+import items from '../data/Items';
 import '../styles/Catalog.css';
 
-function Catalog({ items }) {
+function Catalog() {
+	const params = useParams();
+	const category = params['*'];
+
+	const thisItem = items.find((item) => item.name.toLowerCase() === category);
+
+	const renderedItems = thisItem.list.map((item, key) => {
+		console.log(item.src);
+		return (
+			<img
+				src={item.src}
+				key={key}
+				alt={item.name}
+			/>
+		);
+	});
+
 	return (
 		<div className='catalog-container'>
-			<div className='item'>
-				<img src='' />
-			</div>
+			{renderedItems && <div className='item'>{renderedItems}</div>}
 		</div>
 	);
 }
---
+++ src/data/Items.js
@@ -0,0 +1,28 @@
+import stealth2plus from '../img/drivers/stealth2plus.jpeg';
+import dynapower from '../img/drivers/dynapower.jpeg';
+
+const items = [
+	{
+		name: 'Drivers',
+		list: [
+			{
+				brand: 'TaylorMade',
+				model: 'Stealth 2 Plus',
+				price: '700',
+				rating: '4',
+				reviews: '250',
+				src: stealth2plus,
+			},
+			{
+				brand: 'Wilson',
+				model: 'Dynapower',
+				price: '500',
+				rating: '3',
+				reviews: '470',
+				src: dynapower,
+			},
+		],
+	},
+];
+
+export default items;
