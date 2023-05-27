5564e510dd90d74d4fd9c3337c677994c822e706
Kyle-Close <k.james.close@gmail.com>
2023-05-27 12:25:46 -0700

    Checkout page styling almost done

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
 				/>
 				<Route
 					path='/checkout'
-					element={<Checkout />}
+					element={<Checkout cartItems={customerCart} />}
 				/>
 			</Routes>
 		</div>
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,10 +2,35 @@ import React from 'react';
 
 import '../styles/Checkout.css';
 
-function Checkout() {
+function Checkout({ cartItems }) {
+	const checkoutItems = cartItems.map((item) => {
+		console.log(item);
+		return (
+			<div className='checkout-item'>
+				<img src={item.src} />
+				<div>
+					<h4>{item.brand}</h4>
+					<p>{item.model}</p>
+				</div>
+				<div className='item-quantity-each'>
+					<h4>Quantity</h4>
+					<input
+						type='number'
+						defaultValue={item.count}
+					/>
+					<h4>Each</h4>
+					<p>${item.price}</p>
+				</div>
+				<div className='item-total'>
+					<h4>Total</h4>
+					<p>${item.price * item.count}</p>
+				</div>
+			</div>
+		);
+	});
 	return (
 		<div className='checkout-container-section'>
-			<div className='checkout-items-section'></div>
+			<div className='checkout-items-section'>{checkoutItems}</div>
 			<div className='checkout-total-section'>
 				<h3>Grand Total</h3>
 				<div className='checkout-total-values-section'>
